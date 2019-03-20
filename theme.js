5b865c8063003b41285bebbe20e27cf1220ed1ec
Pablo Bermejo <pablobermejo@Pablos-MacBook-Pro.local>
2019-03-20 01:37:22 +0100

    changed theme

--- theme.js
+++ theme.js
@@ -1,7 +1,9 @@
-export const theme = {
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+// Example theme.js
+export default {
+  // add a custom font
+  // custom colors
+  colors: {
+    text: 'white',
+    background: 'black'
+  }
 }
